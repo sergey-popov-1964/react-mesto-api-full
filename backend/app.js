7d07f5adaed0498d2d1f22a0c9d21b21dbf7b1ac
Sergey Popov <sergeykms@gmail.com>
2021-06-20 21:25:41 +1000

    20_06_2021__9

--- backend/app.js
+++ backend/app.js
@@ -37,6 +37,13 @@ app.use(bodyParser.json());
 
 app.use(requestLogger);
 
+app.get('/crash-test', () => {
+  setTimeout(() => {
+    throw new Error('Сервер сейчас упадёт');
+  }, 0);
+});
+
+
 app.post('/signin', login);
 app.post('/signup', createUser);
 
